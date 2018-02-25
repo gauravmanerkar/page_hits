df6ac0d782e007a6ed6713148f3a7f47fab538c7
Gaurav Manerkar <gauravmanerkar@gmail.com>
2018-02-25 15:29:43 +0530

    Code review

--- src/Form/PageHitsSettingsForm.php
+++ src/Form/PageHitsSettingsForm.php
@@ -132,8 +132,7 @@ class PageHitsSettingsForm extends ConfigFormBase {
   }
 
   public function submitPageHitsClear(array &$form, FormStateInterface $form_state) {
-    return page_hits_flush_all();
+    page_hits_flush_all();
     drupal_set_message($this->t('Page Hits cleared.'));
   }
-
 }
